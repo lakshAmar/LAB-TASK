aa8601907bdc53da131660727d8037a09e7ce6b6
lakshAmar <lakshamarlal@gmail.com>
2026-04-18 21:49:19 +0500

    Update task1.c

--- lab10/task1.c
+++ lab10/task1.c
@@ -1,1 +1,40 @@
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int main() {
+
+    char name[50];
+    int len, i;
+    int valid = 1;
+
+    printf("Enter student name: ");
+    fgets(name, 50, stdin);
+
+    len = strlen(name);
+    if (name[len - 1] == '\n') {
+        name[len - 1] = '\0';
+        len--;
+    }
+
+    if (len < 3 || len > 20)
+        valid = 0;
+
+    if (name[0] == ' ' || name[len - 1] == ' ')
+        valid = 0;
+
+    for (i = 0; i < len; i++) {
+        if (isdigit(name[i]))
+            valid = 0;
+    }
+
+    printf("Length: %d\n", len);
+
+    if (valid == 1)
+        printf("Valid Name\n");
+    else
+        printf("Invalid Name\n");
+
+    return 0;
+}
 

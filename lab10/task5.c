9d66eb5fe6f29ae85ef69cb0f25729a3a7c5c428
lakshAmar <lakshamarlal@gmail.com>
2026-04-18 21:56:32 +0500

    Update task5.c

--- lab10/task5.c
+++ lab10/task5.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char words[6][20];
+    int seen[6] = {0};
+    int i, j, count, len;
+
+    printf("Enter 6 words:\n");
+    for (i = 0; i < 6; i++) {
+        printf("Word %d: ", i + 1);
+        scanf("%s", words[i]);
+    }
+
+    printf("\n--- Word Frequency ---\n");
+    for (i = 0; i < 6; i++) {
+
+        if (seen[i] == 1)
+            continue;
+
+        count = 1;
+        for (j = i + 1; j < 6; j++) {
+            if (strcmp(words[i], words[j]) == 0) {
+                count++;
+                seen[j] = 1;
+            }
+        }
+
+        printf("Word: ");
+        len = strlen(words[i]);
+        for (j = 0; j < len; j++)
+            printf("%c ", words[i][j]);
+
+        printf("- appeared %d time(s)\n", count);
+    }
+
+    return 0;
+}
 

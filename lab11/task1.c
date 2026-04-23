b5adb654fe2be96a84fd591f6530458075ac2b11
lakshAmar <lakshamarlal@gmail.com>
2026-04-23 22:31:09 +0500

    Update task1.c

--- lab11/task1.c
+++ lab11/task1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+int floorPower(int f) {
+ if (f == 1)
+ return 100;
+ return 2 * floorPower(f - 1);
+}
+int totalPower(int floors) {
+ if (floors == 1)
+ return 100;
+ return floorPower(floors) + totalPower(floors - 1);
+}
+int main() {
+ int n;
+ printf("Enter number of floors: ");
+ scanf("%d", &n);
+ printf("Total power consumption: %d kWh\n", totalPower(n));
+ return 0;
+}
+
 

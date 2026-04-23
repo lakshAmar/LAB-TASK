91e9339d45f49b300d1d6d73dd9c7d78d098e70a
lakshAmar <lakshamarlal@gmail.com>
2026-04-23 22:38:37 +0500

    Update task2.c

--- lab11/task 2/task2.c
+++ lab11/task 2/task2.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+int countUpper(char *str) {
+ if (str[0] == '\0')
+ return 0;
+ if (isupper(str[0]))
+ return 1 + countUpper(str + 1);
+ return countUpper(str + 1);
+}
+int countDigits(char *str) {
+ if (str[0] == '\0')
+ return 0;
+ if (isdigit(str[0]))
+ return 1 + countDigits(str + 1);
+ return countDigits(str + 1);
+}
+int main() {
+ char pass[50];
+ printf("Enter password: ");
+ scanf("%s", pass);
+ printf("Uppercase letters: %d\n", countUpper(pass));
+ printf("Digits: %d\n", countDigits(pass));
+ return 0;
+}
 
